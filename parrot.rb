20a976e3c4c4ee146a9e1863d2d1034556f6c603
Melissa Sperry <sperry.mel@gmail.com>
2020-02-12 06:02:04 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,5 +1,5 @@
 def parrot(echo="Bananas taste great!")
-  puts "Banans taste great! #{echo}"
+  puts "Bananas taste great! #{echo}"
 end
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
